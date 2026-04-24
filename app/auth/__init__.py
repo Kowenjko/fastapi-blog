f3208a94f70ff00fc81e9681ad8875500e0bc966
Kowenjko <ortoswt@gmail.com>
2026-04-24 21:01:31 +0300

    add models

---
+++ app/auth/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("PasswordResetToken",)
+
+from .models import PasswordResetToken
---
+++ app/users/repositiries.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class UserRepository:
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create(self, email: str):
+        pass
